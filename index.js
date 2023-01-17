c4ed0403829139717291dfa1627c54a3c474b11c
Endercat <84635059+endercat126@users.noreply.github.com>
2023-01-17 14:37:53 -0500

    something?

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
-function setTheme(theme) {
+function updateTheme() {
     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
     if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.documentElement.classList.add('dark')
     } else {
         document.documentElement.classList.remove('dark')
     }
+}
+
+function setTheme(theme) {
+    var theme_button = document.getElementById("theme_button")
   
     if (theme == 'light') {
         localStorage.theme = 'light'
+        theme_button.classList.remove('fa-sun')
+        theme_button.classList.remove('fa-moon')
+        theme_button.classList.remove('fa-gear')
+        theme_button.classList.add('fa-sun')
     }
 
     if (theme == 'dark') {
         localStorage.theme = 'dark'
+        theme_button.classList.remove('fa-sun')
+        theme_button.classList.remove('fa-moon')
+        theme_button.classList.remove('fa-gear')
+        theme_button.classList.add('fa-moon')
     }
   
     if (theme == 'system') {
         localStorage.removeItem('theme')
+        theme_button.classList.remove('fa-sun')
+        theme_button.classList.remove('fa-moon')
+        theme_button.classList.remove('fa-gear')
+        theme_button.classList.add('fa-gear')
+    }
+
+    updateTheme()
+}
+
+function getTheme() {
+    if (localStorage.theme === 'dark') {
+        return 'dark'
+    } else if (localStorage.theme === 'light') {
+        return 'light'
+    } else {
+        return 'system'
+    }
+}
+
+function cycleTheme() {
+    theme = getTheme
+
+    switch (theme) {
+        case 'light':
+            setTheme('dark')
+            break
+        case 'dark':
+            setTheme('system')
+            break
+        case 'system':
+            setTheme('light')
+            break
+        default:
+            window.alert("Unable to change theme")
     }
 }
