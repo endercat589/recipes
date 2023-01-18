854dc6e5386c714298cb8ebd5960a8ef992a1370
Endercat <84635059+endercat126@users.noreply.github.com>
2023-01-18 00:12:54 -0500

    added some things

--- index.js
+++ index.js
@@ -1,66 +1,11 @@
-function updateTheme() {
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark')
+var mobileNavOpen = false
+
+function toggleMobileNav() {
+    if (mobileNavOpen) {
+        mobileNavOpen = false
+        document.getElementById("mobile_nav").classList.add('h-0')
     } else {
-        document.documentElement.classList.remove('dark')
-    }
-}
-
-function setTheme(theme) {
-    var theme_button = document.getElementById("theme_button")
-  
-    if (theme == 'light') {
-        localStorage.theme = 'light'
-        theme_button.classList.remove('fa-sun')
-        theme_button.classList.remove('fa-moon')
-        theme_button.classList.remove('fa-gear')
-        theme_button.classList.add('fa-sun')
-    }
-
-    if (theme == 'dark') {
-        localStorage.theme = 'dark'
-        theme_button.classList.remove('fa-sun')
-        theme_button.classList.remove('fa-moon')
-        theme_button.classList.remove('fa-gear')
-        theme_button.classList.add('fa-moon')
-    }
-  
-    if (theme == 'system') {
-        localStorage.removeItem('theme')
-        theme_button.classList.remove('fa-sun')
-        theme_button.classList.remove('fa-moon')
-        theme_button.classList.remove('fa-gear')
-        theme_button.classList.add('fa-gear')
-    }
-
-    updateTheme()
-}
-
-function getTheme() {
-    if (localStorage.theme === 'dark') {
-        return 'dark'
-    } else if (localStorage.theme === 'light') {
-        return 'light'
-    } else {
-        return 'system'
-    }
-}
-
-function cycleTheme() {
-    theme = getTheme
-
-    switch (theme) {
-        case 'light':
-            setTheme('dark')
-            break
-        case 'dark':
-            setTheme('system')
-            break
-        case 'system':
-            setTheme('light')
-            break
-        default:
-            window.alert("Unable to change theme")
+        mobileNavOpen = true
+        document.getElementById("mobile_nav").classList.remove('h-0')
     }
 }
